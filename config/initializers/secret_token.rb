3d0b086800516f4d05ac59386b549b1d25254d8c
malluc <malluc_299@hotmail.com>
2011-02-06 20:23:03 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessingApp::Application.config.secret_token = '2b77493cc2383cdabf26879cbcf0daaca27d047e073ce4847327048142c40c3bade8fa8430723887ccfbb124418903ef62a78712c27f0a05f176881858bfb36a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MessingApp::Application.config.session_store :cookie_store, :key => '_messing_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MessingApp::Application.config.session_store :active_record_store
